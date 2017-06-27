fa20ab6b5e0cf64e72b0e3ce4ba110988743c2d2
詹聪聪 <zhancongc@gmail.com>
2017-06-27 15:41:35 +0800

    finished

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, Table, Column, Integer, String, Unicode, UnicodeText
 from sqlalchemy import select, and_, or_, asc, desc
+from config import SQLALCHEMY_DATABASE_URI
 
 Base = declarative_base()
 class Movies(Base):
@@ -10,7 +11,7 @@ class Movies(Base):
     id = Column('id', Integer, primary_key=True)
     url = Column('url', String(128))
     title = Column('title', UnicodeText())
-    image = Column('img', String(128))
+    img = Column('img', String(128))
     year = Column('year', Integer())
     area = Column('area', Unicode(128))
     score = Column('score', Integer)
@@ -22,29 +23,38 @@ class Movies(Base):
     thunder = Column('thunder', String(256))
     magnet = Column('magnet', String(256))
 
-engine = create_engine('mysql+pymysql://root:security@localhost:3306/movie?charset=utf8mb4')
+class Operate(object):
+    def __init__(self):
+        self.engine = create_engine(SQLALCHEMY_DATABASE_URI)
+        self.__init_db__()
+        self.session = self._get_session_()
 
-Base.metadata.bind = engine
-Base.metadata.create_all()
+    def _get_session_(self):
+        Session = sessionmaker(bind=self.engine)
+        session = Session()
+        return session
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    def __init_db__(self):
+        Base.metadata.bind = self.engine
+        Base.metadata.create_all()
 
-obj = Movies(
-    url='http://m.idyjy.com/sub/112.html',
-    title='三不管',
-    image='http://img.idyjy.com/pic/uploadimg/2016-3/112.jpg',
-    year='2008',
-    area='香港',
-    score='4.9分',
-    label='动作 惊悚',
-    director='邱礼涛',
-    actor='林家栋，连凯，田蕊妮，方皓玟，陈望华，黄树棠',
-    imdb='tt1288398',
-    introduction='详细剧情：在城市尽头的破落寨城里聚居着的都是走投无路, 或拥有见不得光的过去的人. 他们干着「黄, 赌, 毒」等不法勾当. 这里有自己的秩序, 律法. 寨城俨如一个「三不管」的地带. 一天, 米奇用警车押解罪犯大海, 却失事闯入寨城, 更被寨城内 的人所扣押. 寨城霸主乌鸦憎恨警察, 认为两人当中必有一人是警察, 并准备处决其中一人. 此时, 寨城内经营妓院的阿玲却指诬诋大海才是警察. 米奇因此而获得 释放, 而大海则继续被扣押.原来, 大海是阿玲多年不见的情人, 阿玲认为大海抛弃了她和女儿阿恩, 所以要让大海吃苦头. 同时, 她希望得到米奇的帮助离开城寨, 因为乌鸦一直对阿恩虎视眈眈. 而阿恩遇到被拘禁的大海, 虽然不知道对方是自己父亲, 却因为一种感觉而决定要协助大海脱险. 与此同时, 寨城发现严重的致命瘟 疫, ZF更借口要屠城阻止瘟疫蔓延. 到底米奇, 大海, 阿玲母女四人能否脱离被屠杀的厄运?而大海和阿恩又会否父女相认?',
-    thunder='thunder://QUFlZDJrOi8vfGZpbGV8ob6159OwvNLUsHd3dy5pZHlqeS5jb23PwtTYob/I/bK7udxEVkS5+tPv1tDX1mNkMS5ybXZifDE5MTIzMzcxMXw2NUU1OUVFQjIxNjExNzE0OEU2NDk0QUU3QzdBQjUxMHxoPTJXSlhSTUtBU1ZRSEozM0laUFhHVklGUlVCTzZaT0dSfC9aWg==',
-    magnet=''
-)
-session.add(obj)
-session.flush()
-session.commit()
+    def insert(self, movie):
+        obj = Movies(
+            url = movie['url'],
+            title = movie['title'],
+            img = movie['img'],
+            year = movie['year'],
+            area = movie['area'],
+            score = movie['score'],
+            label = movie['label'],
+            director = movie['director'],
+            actor = movie['actor'],
+            imdb = movie['imdb'],
+            introduction = movie['introduction'],
+            thunder = movie['thunder'],
+            magnet = movie['magnet']
+        )
+        self.session.add(obj)
+        self.session.flush()
+        self.session.commit()
+
--- run.py
+++ run.py
@@ -1,17 +1,26 @@
+# -*- coding: utf-8 -*-
 from parse import Handler
+from models import Operate
+from config import BASE_URL
 
-url = 'http://m.idyjy.com/sub/'
+# get movie data
+base_url = BASE_URL
 movie_list = []
-# max(i) = 23576
-for i in range(111, 114):
+# 该网站目前最大的电影序号为23576
+for i in range(21022, 21024):
     payload = (str(i) + '.html')
     try:
-        page = Handler(url+payload, '')
+        page = Handler(base_url+payload, '')
         movie_list.append(page.movie)
     except Exception as e:
         print('url: ' + url+payload)
         print('error: {0}'.format(e))
 
-print(movie_list)
+print('Get movie successfully!')
 
-# url='http://m.idyjy.com/sub/112.html',title='三不管',image='http://img.idyjy.com/pic/uploadimg/2016-3/112.jpg',year=2008,area='香港',score=4.9,label='动作 惊悚',director='邱礼涛',actor='林家栋，连凯，田蕊妮，方皓玟，陈望华，黄树棠',imdb='tt1288398',introduction='详细剧情：在城市尽头的破落寨城里聚居着的都是走投无路, 或拥有见不得光的过去的人. 他们干着「黄, 赌, 毒」等不法勾当. 这里有自己的秩序, 律法. 寨城俨如一个「三不管」的地带. 一天, 米奇用警车押解罪犯大海, 却失事闯入寨城, 更被寨城内 的人所扣押. 寨城霸主乌鸦憎恨警察, 认为两人当中必有一人是警察, 并准备处决其中一人. 此时, 寨城内经营妓院的阿玲却指诬诋大海才是警察. 米奇因此而获得 释放, 而大海则继续被扣押.原来, 大海是阿玲多年不见的情人, 阿玲认为大海抛弃了她和女儿阿恩, 所以要让大海吃苦头. 同时, 她希望得到米奇的帮助离开城寨, 因为乌鸦一直对阿恩虎视眈眈. 而阿恩遇到被拘禁的大海, 虽然不知道对方是自己父亲, 却因为一种感觉而决定要协助大海脱险. 与此同时, 寨城发现严重的致命瘟 疫, ZF更借口要屠城阻止瘟疫蔓延. 到底米奇, 大海, 阿玲母女四人能否脱离被屠杀的厄运?而大海和阿恩又会否父女相认?',thunder='thunder://QUFlZDJrOi8vfGZpbGV8ob6159OwvNLUsHd3dy5pZHlqeS5jb23PwtTYob/I/bK7udxEVkS5+tPv1tDX1mNkMS5ybXZifDE5MTIzMzcxMXw2NUU1OUVFQjIxNjExNzE0OEU2NDk0QUU3QzdBQjUxMHxoPTJXSlhSTUtBU1ZRSEozM0laUFhHVklGUlVCTzZaT0dSfC9aWg==',magnet=''
+# insert into database
+operate = Operate()
+for i in movie_list:
+	operate.insert(i)
+
+print('Insert to mysql successfully!')
